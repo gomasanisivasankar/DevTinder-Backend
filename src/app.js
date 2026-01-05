34a17559ac7aebc3085751d876f339d48307487d
siva <ssankar6281@gmail.com>
2026-01-05 23:44:38 +0530

    crud operation

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express=require("express")
 const app=express()
 
-app.use("/",(req,res)=>{
-    res.send("Hello from the Dashboard")
+app.get("/user",(req,res)=>{
+    res.send({firstname:"Sivasankar",lastname:"Gomasani"})   
 })
+
+app.post("/user",(req,res)=>{
+    res.send("sav data into the database")
+})
+app.delete("/user",(req,res)=>{
+    res.send("user data deleted successfully")
+})
+// This will match all HTTP methods API call to /test
 app.use("/test",(req,res)=>{
     res.send("Hello from the server")
 })
+
+
 app.listen(3000,()=>{
     console.log("server is successfully listening on the port 3000")
 })
