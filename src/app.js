77d4b9575b231f38d9b308badc693d08a3a8bc6b
siva <ssankar6281@gmail.com>
2026-01-07 19:55:41 +0530

    commmit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,29 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const app = express();
 
-app.use("/user",(req,res,next)=>{
-    console.log("Handling first route!!")
-   
-    next()
-    //  res.send("first Response")
-},(req,res)=>{
-    console.log("Handling second route")
-    // res.send("second route")
-    next()
+app.use("/", (req, res) => {
+  res.send("Handling the / routes");
+});
+
+app.use("/", (req, res) => {
+    res.json({message: "Response from /users"})
 })
 
-app.listen(3000,()=>{
-    console.log("server is successfully listening on the port 3000")
-})
+app.get("/user", (req, res) => {
+  console.log("Handling first route!!");
+  res.send("first Response");
+});
+
+app.post("/user/:id", (req, res) => {
+  console.log("Handling first route!!");
+  res.send("first Response");
+});
+
+app.get("/product", (req, res) => {
+  console.log("Handling first route!!");
+  res.send("first Response");
+});
+
+app.listen(3000, () => {
+  console.log("server is successfully listening on the port 3000");
+});
