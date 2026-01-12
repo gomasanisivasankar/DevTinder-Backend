3da8606fabb8d08649cb6a9994819aec13beaa14
siva <ssankar6281@gmail.com>
2026-01-12 12:28:29 +0530

    added schema validation

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ const User = require("./models/user");
 const connectDB = require("./config/database");
 const app = express();
 app.use(express.json())
+
+
 app.post("/signup", async (req, res) => {
-  
+
   const user = new User(req.body);
   try {
     await user.save();
@@ -16,13 +18,69 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async(req,res)=>{
+  const userEmail=req.body.emailId;
+
+  try{
+    const users=await User.find({emailId:userEmail})
+   if(users.length===0){
+    res.status(404).send("User Not Found")
+   }
+   else{
+     res.send(users)
+   }
+  }
+  catch(err){
+    res.status(400).send("something went wrong"+err.message)
+  }
+})
+
+app.delete("/user", async(req,res)=>{
+  const userId=req.body.userId
+  try{
+    const user=await User.findByIdAndDelete(userId)
+    res.send("User Deleted Successfully")
+  }catch(err){
+    res.status(400).send("something went wrong"+err.message)
+  }
+})
+
+app.patch("/user", async(req,res)=>{
+  const userId=req.body.userId;
+  const data=req.body;
+  try{
+    const user= await User.findByIdAndUpdate(userId,data,{returnDocument:"after",runValidators:true})
+    console.log(user)
+    res.send("User updated successfully")
+  }
+  catch(err){
+    res.status(400).send("Update Failed"+err.message)
+  }
+})
+
+
+
+app.get("/feed",async(req,res)=>{
+  try{
+    const users=await User.find({})
+    res.send(users)
+  }
+  catch(err){
+    res.status(400).send("something went wrong"+err.message)
+  }
+})
+
+
 connectDB()
-  .then(() => {
+  .then(async () => {
+    await User.syncIndexes();
     console.log("Database is successfully established");
     app.listen(3000, () => {
       console.log("server is successfully listening on the port 3000");
     });
   })
-  .catch((err) => {
+  .catch(err => {
     console.log("Database is not connected");
+    console.error(err);
   });
+
--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,54 @@ const mongoose=require('mongoose')
 
 const userSchema=new mongoose.Schema({
     firstName:{
-        type:String
+        type:String,
+        required:true,
+        minLength:4,
+        maxLength:50
     },
     lastName:{
         type:String
     },
     emailId:{
-        type:String
+        type:String,
+        required:true,
+        lowercase:true,
+        unique: true,
+        trim:true
     },
     password:{
-        type:String
+        type:String,
+        required:true,
+        
     },
     age:{
-        type:Number
+        type:Number,
+        min:18,
+        
     },
     gender:{
-        type:String
+        type:String,
+        validate(value){
+            if(!["male","female","others"].includes(value)){
+             throw new Error("Gender data is  not valid")
+            }
+        }
+    },
+    photoURL:{
+        type:String,
+        default:"",
+    },
+    about:{
+        type:String,
+        default:"This is all about of the User!"
+    },
+    skills:{
+        type:[String]
     }
-})
+
+},
+{ timestamps: true }
+)
 
 module.exports=mongoose.model("User",userSchema)
+
