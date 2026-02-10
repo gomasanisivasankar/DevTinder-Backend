008f5b878fe1ad5280c7a845cc9b4a60c050a883
siva <ssankar6281@gmail.com>
2026-02-10 23:36:32 +0530

    added to mondo

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const connectDB=async()=>{
-    await mongoose.connect("mongodb+srv://sivasankar:DPZll5Rqxk1gJeJ4@namastedev.6ulpizu.mongodb.net/devTinder")
+    await mongoose.connect(process.env.MONGO_URI)
 
 }
 module.exports=connectDB;
