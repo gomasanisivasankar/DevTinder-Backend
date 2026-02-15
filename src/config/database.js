dbfe0e1cb606bb255c6f3366e6504a24dccc3476
siva <ssankar6281@gmail.com>
2026-02-15 10:30:04 +0530

    mongo db added

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose')
 
 const connectDB=async()=>{
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect("mongodb+srv://sivasankar:gC3YQjl8f5kkPnun@namastedev.6ulpizu.mongodb.net/devTinder")
 
 }
 module.exports=connectDB;
